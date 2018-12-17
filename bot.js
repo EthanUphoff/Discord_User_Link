a817834fa93aa7412fc87279d1e1736a9d4b095c
EthanUphoff <ethan.uphoff@gmail.com>
2018-12-17 03:51:32 -0600

    Refactored the embedding.

--- bot.js
+++ bot.js
@@ -12,41 +12,33 @@ bot.on('ready', function(){
 
 bot.on('message', function(msg) {
   if(msg.content == 'What is my url?'){
-    msg.channel.send({embed: {
-      color: Math.floor(Math.random() * 16777214) + 1,
-      author: {
-        name: msg.author.username,
-        icon_url: msg.author.avatarURL
-      },
-      title: 'https://discordapp.com/users/' + msg.author.id,
-      url: 'https://discordapp.com/users/' + msg.author.id
-    }})
-    return;
-  }
-  if(msg.content.startsWith(prefix)){
+    urlstuff(msg, msg.author)
+  } else if(msg.content.startsWith(prefix)){
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift();
     if(command == 'help'){
       msg.channel.send('You can either type ``What is my url?`` or ``url?<mentions>``');
-      return;
-    }
-    if(command == ''){
+    } else if(command == ''){
       msg.channel.send('Must include a mention.')
     } else if (msg.mentions.users.size < 1) {
       msg.channel.send('No mentions found, make sure you are actually mentioning someone.');
     } else {
       var dismentions = msg.mentions.users
       dismentions.forEach(function(item, index, array){
-        msg.channel.send({embed: {
-          color: Math.floor(Math.random() * 16777214) + 1,
-          author: {
-            name: item.username,
-            icon_url: item.avatarURL
-          },
-          title: 'https://discordapp.com/users/' + item.id,
-          url: 'https://discordapp.com/users/' + item.id
-        }})
+          urlstuff(msg, item);
       })
     }
   }
 })
+
+function urlstuff(msg, items){
+  msg.channel.send({embed: {
+    color: Math.floor(Math.random() * 16777214) + 1,
+    author: {
+      name: items.username,
+      icon_url: items.avatarURL
+    },
+    title: 'https://discordapp.com/users/' + items.id,
+    url: 'https://discordapp.com/users/' + items.id
+  }})
+}
