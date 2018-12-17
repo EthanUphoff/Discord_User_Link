cc66c10c041eaa6c13d3486bd3b475fdef65c2b0
EthanUphoff <ethan.uphoff@gmail.com>
2018-12-17 02:36:03 -0600

    first commit

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+var prefix = "url?"
+
+bot.login('NTI0MTI0MjA3MTk4NjMzOTg0.DvjgTg.FQyVxmdWpHeO2pkeKN4tk8Q6LNk')
+
+bot.on('ready', function(){
+  console.log('The bot is online!')
+  bot.user.setActivity(`on ${bot.guilds.size} servers`)
+})
+
+bot.on('message', function(msg) {
+  if(msg.content == 'What is my url?'){
+    msg.channel.send('https://discordapp.com/users/' + msg.author.id);
+  }
+  if(msg.content.startsWith(prefix)){
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift();
+    if(command == ''){
+      msg.channel.send('Must include a mention.')
+    } else if (msg.mentions.users.size < 1) {
+      msg.channel.send('No mentions found, make sure you are actually mentioning someone.');
+    } else {
+      var dismentions = msg.mentions.users
+      dismentions.forEach(function(item, index, array){
+        msg.channel.send(item.username + ': ' + 'https://discordapp.com/users/' + item.id);
+      })
+    }
+  }
+})
